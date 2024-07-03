74f061f1b4d0badfaea01a79f2eaa94529b89da6
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2024-07-04 00:27:11 +0530

    dashboard-connected-to=posthog

---
+++ app/PostHogPageView.tsx
@@ -0,0 +1,33 @@
+// app/PostHogPageView.tsx
+'use client'
+
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { usePostHog } from 'posthog-js/react';
+
+export default function PostHogPageView() : null {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
+  useEffect(() => {
+    console.log("outisde");
+    
+    // Track pageviews
+    if (pathname && posthog) {
+        console.log("inside");
+        
+      let url = window.origin + pathname
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`
+      }
+      posthog.capture(
+        '$pageview',
+        {
+          '$current_url': url,
+        }
+      )
+    }
+  }, [pathname, searchParams, posthog])
+  
+  return null
+}
